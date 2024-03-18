05e98e083b1038b9eafd24eacb83fc724727c0f6
Md.Arif <mohammadarif.ma02@gmail.com>
2024-03-18 08:52:54 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,386 @@
+// let FCC = "freeCOdeCamo";
+
+// let Fact = "is awesome!";
+
+// console.log(FCC , Fact);
+
+// const sum = 10 + 10 ;
+
+// console.log(sum);
+
+// const myVar = 10-5;
+
+// console.log(myVar);
+
+// const product = 8 * 8;
+
+// console.log(product);
+
+// const vag = 66 / 33 ;
+
+// console.log(vag);
+
+// let myVar = 87;
+
+// myVar++;
+
+// console.log(myVar);
+
+// let i = 10;
+
+// i--
+
+// console.log(i);
+
+// const product = 2.0 * 2.5;
+
+// console.log(product);
+
+// const dosomik = 4.4 / 2;
+
+// console.log(dosomik);
+
+//  const remaider = 10 % 40;
+
+//  console.log(remaider);
+
+// let a = 3;
+
+// let b = 17;
+
+// let c = 12;
+
+// a = a += 12
+
+// b = b += 9
+
+// c = c += 7;
+
+// console.log(a,b,c);
+
+// let a = 11;
+
+// let b = 9;
+
+// let c = 3;
+
+// a = a -= 6;
+
+// b = b -= 15;
+
+// c = c -= 1;
+
+// console.log(a,b,c);
+
+// let a = 5;
+
+// let b = 12;
+
+// let c = 4.6;
+
+// a = a *= 5;
+
+// b = b *= 3;
+
+// c = c *= 10;
+
+// console.log(a, b, c);
+
+// let a = 48;
+
+// let b = 108;
+
+// let c = 33;
+
+// a = a /= 12;
+
+// b = b /= 4;
+
+// c = c /= 11;
+
+// console.log(a,b,c);
+
+// const nam =  "alan said \"peter is learning JavaScript\". string inside  \" double quotes\".";
+
+// console.log(nam);
+
+// let FirstLine = " my name is Arif";
+
+// let SecondLine = " this is arif";
+
+// let  ThirdLine = " how are you";
+
+// let myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
+
+// console.log(FirstLine);
+
+// const plus = "My name is Arif , "+" I am Fine ";
+
+// console.log(plus);
+
+// let myStr = "This is the first sentence";
+
+// myStr += " This is the second";
+
+// console.log(myStr);
+
+// const myName = "abc";
+
+// const myStr = "Hello , our name " + myName + ", how are you";
+
+// console.log(myStr);
+
+// const anAdjective = " awesome!";
+
+// let ourStr = "freeCodeCamp is";
+
+// ourStr +=  anAdjective;
+
+// console.log(ourStr);
+
+// var lastNameLength = 0;
+
+// var lastName = "lovelace";
+
+// lastNameLength = lastName.length
+
+// console.log(lastNameLength);
+
+// let firstLetterOfLastName = "l";
+
+// const lastName = "lovelace"
+
+// firstLetterOfLastName = lastName[5];
+
+//console.log(firstLetterOfLastName);
+
+// let myStr = "jello World";
+
+// myStr = "Hello World";
+
+// console.log(myStr);
+
+// const firstName = "Arif Mohammad"
+
+// const secondLetterOfFirstName = firstName[[3],[5]];
+
+// console.log(secondLetterOfFirstName);
+
+// const firstName = "Arif Mohammad"
+
+// const secondLetterOfFirstName = firstName[firstName.length -2];
+
+// console.log(secondLetterOfFirstName);
+
+// Setup
+// const lastName = "Lovelace";
+
+// Only change code below this line
+// const secondToLastLetterOfLastName = lastName[lastName.length -2]; // Change this line
+
+// console.log(secondToLastLetterOfLastName);
+
+// const myNoun = "dog";
+
+// const myAdjective = "big";
+
+// const myVerb = "ran";
+
+// const myAdverb = "quickly"
+
+// const wordBlanks = " It Was Really " + "dog" + ", and We " + "big" +" ourselves " + "ran" + "sill "+ "quickly." + " ";
+
+// console.log(wordBlanks);
+
+// var myArray = ["Arif", 23];
+
+// console.log(myArray);
+
+// const teams = [['Bulls', 23], ['white sox', 45],['white sox', 45],['white sox', 45]];
+
+// console.log(teams);
+
+
+// const myArray = [50, 60, 70];
+
+// const myData = (myArray [2]);
+
+// console.log(myData);
+
+
+// const ourArray = [50, 40, 30];
+
+// ourArray[0] = 20;
+
+// console.log(ourArray);
+
+// অপেক্ষা :myDataসমান হওয়া উচিত 8।
+// const myArray = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9],
+//     [[10, 11, 12], 13, 14],
+//   ];
+  
+//   const myData = myArray[2][1];
+
+
+// const myArray = [["John", 23], ["cat", 2]];
+
+// myArray.push(["dog", 3]);
+
+// console.log(myArray);
+
+
+// const myArray = [["John", 23], ["cat", 2]];
+
+
+// myArray.push = (["bangladesh", "lalala", 20])
+
+// console.log(myArray);
+
+
+// const myArray = [["john", 23], ["cat", 2]];
+
+// const removedFromMyArray = myArray.pop('llllll');
+
+// console.log(removedFromMyArray);
+
+// const myArray = [["john", 23], ["dog", 3]];
+
+// const removedFromMyArray = myArray.shift();
+
+// console.log(removedFromMyArray);
+
+// const myArray = [["john", 23], ["dog", 3]];
+
+// const removedFromMyArray = myArray.shift();
+
+// const addedFromMyArray =  myArray.unshift(["paul", 35]);
+
+// console.log(myArray);
+// const myList = [["chocolate Bar", 5], ["Orange", 8], ["mango", 10], ["Banana", 12], ["Apple", 15]];
+
+// console.log(myList);
+
+
+// function reusableFunction (){
+//     console.log("Hi World");
+//   }
+
+//   reusableFunction();
+
+
+// function  functionWithArgs(item1,item2) {
+//     console.log(item1 + item2);
+// }
+
+
+// functionWithArgs(1,2);
+
+// function  functionWithArgs(a,b) {
+//     console.log(a + b);
+// }
+
+
+// functionWithArgs(7,9);
+
+
+// function plusThree (num) {
+//     return num + 3 ;
+
+// }
+
+
+// const answer = plusThree(5);
+
+
+// console.log(plusThree);
+
+
+
+
+// function timesFive (gun){
+//     return gun * 5
+// }
+
+// const answer = timesFive(5);
+
+// console.log(answer);
+
+
+// function timesFive1 (gun){
+//     return gun * 2
+// }
+
+// const answer1 = timesFive1(5);
+
+// console.log(answer1);
+
+
+// function timesFive2 (gun){
+//     return gun * 0
+// }
+
+// const answer2 = timesFive2(0);
+
+// console.log(answer2);
+
+
+// Declare the myGlobal variable below this line
+
+
+// function fun1() {
+//     // Assign 5 to oopsGlobal here
+  
+//   }
+  
+//   // Only change code above this line
+  
+//   function fun2() {
+//     let output = "";
+//     if (typeof myGlobal != "undefined") {
+//       output += "myGlobal: " + myGlobal;
+//     }
+//     if (typeof oopsGlobal != "undefined") {
+//       output += " oopsGlobal: " + oopsGlobal;
+//     }
+//     console.log(output);
+//   }
+
+
+// function myLocalScope() {
+//     // Only change code below this line
+  
+//     console.log('inside myLocalScope', myVar);
+//   }
+//   myLocalScope();
+  
+//   // Run and check the console
+//   // myVar is not defined outside of myLocalScope
+//   console.log('outside myLocalScope', myVar);
+
+
+// var outerWear = "T-Shirt";
+
+// function myOutfit() {
+//   var outerWear = "sweater";
+//   return outerWear;
+// }
+
+
+
+// console.log(myOutfit());
+
+// let sum = 0;
+
+// function addThree() {
+//   sum = sum + 3;
+// }
+
+// function addFive  (){
+//   sum += 5 ;
+// }
+
+
+// console.log(addFive());
